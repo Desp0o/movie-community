f27f990da567c6349707f9a3f0cba67d268cdc86
Despo <tornike.despotashvili@gmail.com>
2024-03-24 17:57:33 +0400

    create toggler

--- src/components/navbar/UserDash.tsx
+++ src/components/navbar/UserDash.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react"
 import { logOutIcon } from "../../assets/svg/logOut"
 import { moonIcon } from "../../assets/svg/moonIcon"
 import UserDashItem from "./UserDashItem"
 
 const UserDash = () => {
+    const [isDarkMode, setDarkMode] = useState(false)
+
+    const toggleHandler = () => {
+        setDarkMode(!isDarkMode)
+    }
+
+    const toggler = () => {
+        return <div className="toggler" onClick={toggleHandler}>
+            <div className={isDarkMode ? "toggler_thub active" : "toggler_thub"}/>
+        </div>
+    }
+
   return (
     <div className="user_dash">
-        {moonIcon}
-        <UserDashItem icon={moonIcon} text="Dark Mode"/>
+        <UserDashItem icon={moonIcon} text="Dark Mode" toggler={toggler}/>
         <UserDashItem icon={logOutIcon} text="Log Out"/>
     </div>
   )
--- src/components/navbar/UserDashItem.tsx
+++ src/components/navbar/UserDashItem.tsx
@@ -3,13 +3,16 @@ import React, { ReactNode } from "react"
 interface UserDashItemProps {
     icon: ReactNode;
     text: string;
+    toggler?: () => JSX.Element | null;
 }
 
-const UserDashItem:React.FC<UserDashItemProps> = ({icon, text}) => {
+const UserDashItem:React.FC<UserDashItemProps> = ({icon, text, toggler}) => {
   return (
     <div className="user_dash_item">
         {icon}
         <p>{text}</p>
+
+{toggler && toggler()}
     </div>
   )
 }
