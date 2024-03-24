a92b52a6ecd9dfb3ac3d89ee14ca56e50299a2e4
Despo <tornike.despotashvili@gmail.com>
2024-03-24 12:47:04 +0400

    create feed page and setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import Feed from "./Feed"
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+      </Routes>
     </>
   )
 }
