c6b9b028ee6baf812c66e500c7fe5fc48c922292
Despo <tornike.despotashvili@gmail.com>
2024-03-25 22:32:11 +0400

    create google button

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,12 +1,25 @@
+import { googleAuthIcon } from "../../assets/svg/google"
+import { useGoogleLogIn } from "../../hooks/useGoogleAuth"
 import "./login.css"
 
 const Login = () => {
+
+  const { googleLogIn } = useGoogleLogIn()
+
   return (
     <div className="login_modal">
         <div className="log_modal_block1">
             <p>Log In</p>
             <p>By continuing, you agree to our User Agreement and <br/> acknowledge that you understand the Privacy Policy.</p>
         </div>
+
+        <div className="google_login">
+          <div className="google_login_btn" onClick={googleLogIn}>
+            {googleAuthIcon}
+            Google
+          </div>
+        </div>
+        
     </div>
   )
 }
--- src/hooks/useGoogleAuth.tsx
+++ src/hooks/useGoogleAuth.tsx
@@ -20,5 +20,5 @@ export const useGoogleLogIn = () => {
         }
       };
       
-    return googleLogIn
+    return {googleLogIn}
 }
