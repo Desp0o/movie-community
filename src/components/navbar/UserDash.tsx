bec49ae7267a0cd98c49abaaa4d7d6806a34adbf
Despo <tornike.despotashvili@gmail.com>
2024-03-28 11:38:43 +0400

    update user dash item and add logout hook on click

--- src/components/navbar/UserDash.tsx
+++ src/components/navbar/UserDash.tsx
@@ -5,9 +5,11 @@ import { useDispatch } from "react-redux"
 import { setDarkBG } from "../../Redux/DarkModeSlicer"
 import { useDarkModeHook } from "../../hooks/useDarkModeHook"
 import { profileIcon } from "../../assets/svg/profileIcon"
+import { useLogOut } from "../../hooks/useLogOut"
 
 const UserDash = () => {
     const { isDark } = useDarkModeHook()
+    const { handleLogout } = useLogOut()
     const dispatch = useDispatch()
 
     const toggleHandler = () => {
@@ -24,7 +26,7 @@ const UserDash = () => {
     <div className="user_dash">
         <UserDashItem icon={profileIcon} text="Profile"/>
         <UserDashItem icon={moonIcon} text="Dark Mode" toggler={toggler}/>
-        <UserDashItem icon={logOutIcon} text="Log Out"/>
+        <UserDashItem icon={logOutIcon} text="Log Out" funName={handleLogout}/>
     </div>
   )
 }
--- src/components/navbar/UserDashItem.tsx
+++ src/components/navbar/UserDashItem.tsx
@@ -5,12 +5,13 @@ interface UserDashItemProps {
   icon: ReactNode;
   text: string;
   toggler?: () => JSX.Element | null;
+  funName?: () => void
 }
 
-const UserDashItem: React.FC<UserDashItemProps> = ({ icon, text, toggler }) => {
+const UserDashItem: React.FC<UserDashItemProps> = ({ icon, text, toggler, funName }) => {
   const { isDark } = useDarkModeHook()
   return (
-    <div className={isDark ? "user_dash_item dark" : "user_dash_item"}>
+    <div className={isDark ? "user_dash_item dark" : "user_dash_item"} onClick={funName}>
       {icon}
       <p>{text}</p>
 
