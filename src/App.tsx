d0aa176ddc5c940020b9b10f50bba325aa28612b
Despo <tornike.despotashvili@gmail.com>
2024-03-28 11:31:06 +0400

    fetch user when reload

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,40 @@ import Feed from "./Feed"
 import Navbar from "./components/navbar/navbar"
 import { useDarkModeHook } from "./hooks/useDarkModeHook"
 import "./index.css"
-import { useEffect } from "react"
+import { useEffect, useLayoutEffect, useState } from "react"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { app } from "./components/firebase/firebaseConfig"
+import { setUser } from "./Redux/userSlicer"
+import { useDispatch } from "react-redux"
 
 
 function App() {
   const {isDark} = useDarkModeHook()
+  const [isLoading, setLoading] = useState(false)
+  const dispatch = useDispatch()
 
   useEffect(()=>{
     Boolean(localStorage.getItem('darkMode'))    
   },[])
 
+  useLayoutEffect(() => {
+    setLoading(true)
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser({name: user.displayName, avatar: user.photoURL}))
+        setLoading(false)
+      }else {
+        setLoading(false)
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  if(isLoading){
+    return <div className="">LOADINNNG</div>
+  }
 
   return (
     <div className={isDark ? "app darkMode" : "app"}>
