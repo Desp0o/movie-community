03d7fc62c36f91c15096988d4fe29db303a7ff76
Despo <tornike.despotashvili@gmail.com>
2024-03-28 15:39:39 +0400

    add close function on lgoin modal

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,23 @@
+import { useDispatch } from "react-redux"
+import { xIcon } from "../../assets/svg/Xicon"
 import { googleAuthIcon } from "../../assets/svg/google"
 import { useDarkModeHook } from "../../hooks/useDarkModeHook"
 import { useGoogleLogIn } from "../../hooks/useGoogleAuth"
 import "./login.css"
+import { setModalVisible } from "../../Redux/loginModalSlicer"
 
 const Login = () => {
-
+  const dispatch = useDispatch()
   const { googleLogIn } = useGoogleLogIn()
   const { isDark } = useDarkModeHook()
 
+  const modalCloser = () => {
+    dispatch(setModalVisible(false))
+  }
+
   return (
     <div className={isDark ? "login_modal dark" : "login_modal"}>
+      <div className={isDark ? "x_icon_login_modal dark" : "x_icon_login_modal"} onClick={modalCloser}>{xIcon}</div>
         <div className="log_modal_block1">
             <p>Log In</p>
             <p>By continuing, you agree to our User Agreement and <br/> acknowledge that you understand the Privacy Policy.</p>
