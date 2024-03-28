14a3d60e0f03d45bb0f589d19b9665e9607fac20
Despo <tornike.despotashvili@gmail.com>
2024-03-28 13:54:05 +0400

    update login modal to dark mode

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,13 +1,15 @@
 import { googleAuthIcon } from "../../assets/svg/google"
+import { useDarkModeHook } from "../../hooks/useDarkModeHook"
 import { useGoogleLogIn } from "../../hooks/useGoogleAuth"
 import "./login.css"
 
 const Login = () => {
 
   const { googleLogIn } = useGoogleLogIn()
+  const { isDark } = useDarkModeHook()
 
   return (
-    <div className="login_modal">
+    <div className={isDark ? "login_modal dark" : "login_modal"}>
         <div className="log_modal_block1">
             <p>Log In</p>
             <p>By continuing, you agree to our User Agreement and <br/> acknowledge that you understand the Privacy Policy.</p>
