c7fbe9b2fea36b92941c51935eae7e16979d2254
Despo <tornike.despotashvili@gmail.com>
2024-04-05 11:55:16 +0400

    create left menu close on backdrop click

--- src/components/leftNavigation/LeftNavigation.tsx
+++ src/components/leftNavigation/LeftNavigation.tsx
@@ -1,10 +1,11 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { homeIocn } from "../../assets/svg/homeIcon"
 import { privacyIcon } from "../../assets/svg/privacyIcon"
 import LeftNavItem from "./LeftNavItem"
 import LeftNavSections from "./LeftNavSections"
 import "./LeftNavigation.css"
 import { useDarkModeHook } from "../../hooks/useDarkModeHook"
+import { setLeftMenuState } from "../../Redux/leftMenuSlicer"
 
 interface RootState {
   leftMenuStore:{
@@ -16,18 +17,28 @@ const LeftNavigation = () => {
 
   const isOpen = useSelector((state: RootState) => state.leftMenuStore.isLeftMenuOpen)
   const { isDark } = useDarkModeHook()
+  const dispatch = useDispatch()
+
+  const backdropClickHandler = () => {
+    dispatch(setLeftMenuState(false))
+  }
 
   return (
-<div className={isOpen ? (isDark ? 'left_nav active dark' : 'left_nav active') : (isDark ? 'left_nav dark' : 'left_nav')}>
+    <>
+      <div className={isOpen ? 'lef_menu_backdrop active' : 'lef_menu_backdrop'} 
+        onClick={backdropClickHandler}
+      />
+
+      <div className={isOpen ? (isDark ? 'left_nav active dark' : 'left_nav active') : (isDark ? 'left_nav dark' : 'left_nav')}>
         <LeftNavSections>
             <LeftNavItem title="Home" icon={homeIocn} path="/"/>
         </LeftNavSections>
 
         <LeftNavSections>
             <LeftNavItem title="Privacy Policy" icon={privacyIcon} path="/pages/privacy"/>
-        </LeftNavSections>
-        
-    </div>
+        </LeftNavSections>  
+      </div>
+    </>
   )
 }
 
