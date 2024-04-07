d1df9084fea076fee1f53821a29ba82e0f45de06
Despo <tornike.despotashvili@gmail.com>
2024-04-07 16:16:13 +0400

    update feed

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -1,40 +1,38 @@
-import PageLayout from "./components/pageLayout/PageLayout"
-import SinglePostComp from "./components/singlePostComp/SinglePostComp"
-import avatar from "./assets/Avatar.webp"
-import userav from "./assets/images.jpeg"
+import PageLayout from "./components/pageLayout/PageLayout";
+import SinglePostComp from "./components/singlePostComp/SinglePostComp";
+import avatar from "./assets/Avatar.webp";
+import userav from "./assets/images.jpeg";
+import "./Feed.css";
 
 const arr = [
   {
-    title:'this is first post',
-    name:"tornike despotashvili",
+    title: "this is first post",
+    name: "tornike despotashvili",
     avatar: avatar,
   },
   {
-    title:'second post',
-    name:"koka kvirikashvili",
+    title: "second post",
+    name: "koka kvirikashvili",
     avatar: userav,
-  }
-]
+  },
+];
 
 const Feed = () => {
   return (
-    <>
-      <PageLayout>
-
-        {arr.map( (post) => {
-          return(
-            <SinglePostComp 
+    <PageLayout>
+      <div className="feed">
+        {arr.map((post) => {
+          return (
+            <SinglePostComp
               authorName={post.name}
               authorAvatar={post.avatar}
               postTitle={post.title}
             />
-          )
+          );
         })}
+      </div>
+    </PageLayout>
+  );
+};
 
-      
-      </PageLayout>
-    </>
-  )
-}
-
-export default Feed
+export default Feed;
