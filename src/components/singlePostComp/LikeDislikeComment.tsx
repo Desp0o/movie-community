493a4c6fb161f04e44bd033c0ab40fcc6c04cdf9
Despo <tornike.despotashvili@gmail.com>
2024-04-07 22:31:40 +0400

    add like dislike

--- src/components/singlePostComp/LikeDislikeComment.tsx
+++ src/components/singlePostComp/LikeDislikeComment.tsx
@@ -1,13 +1,19 @@
 import { arrowDislike } from "../../assets/svg/arrowDislike"
 import { arrowLike } from "../../assets/svg/arrowLike"
+import { commentsIcon } from "../../assets/svg/commentsIcon"
 
 const LikeDislikeComment = () => {
   return (
-    <div>
+    <div className="likeDislikeComment_container">
         <div className="like_dislike">
             {arrowLike}
             <p>0</p>
             {arrowDislike}
+        </div>
+
+        <div className="single_post_comments">
+            {commentsIcon}
+            <p>0</p>
         </div>
     </div>
   )
