534da8e641a9db6012cf00acfa93fcfd35a3286d
Despo <tornike.despotashvili@gmail.com>
2024-04-08 15:40:19 +0400

    add backdrop on login popup

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,26 +1,36 @@
-import { useDispatch } from "react-redux"
-import { xIcon } from "../../assets/svg/Xicon"
-import { googleAuthIcon } from "../../assets/svg/google"
-import { useDarkModeHook } from "../../hooks/useDarkModeHook"
-import { useGoogleLogIn } from "../../hooks/useGoogleAuth"
-import "./login.css"
-import { setModalVisible } from "../../Redux/loginModalSlicer"
+import { useDispatch } from "react-redux";
+import { xIcon } from "../../assets/svg/Xicon";
+import { googleAuthIcon } from "../../assets/svg/google";
+import { useDarkModeHook } from "../../hooks/useDarkModeHook";
+import { useGoogleLogIn } from "../../hooks/useGoogleAuth";
+import "./login.css";
+import { setModalVisible } from "../../Redux/loginModalSlicer";
 
 const Login = () => {
-  const dispatch = useDispatch()
-  const { googleLogIn } = useGoogleLogIn()
-  const { isDark } = useDarkModeHook()
+  const dispatch = useDispatch();
+  const { googleLogIn } = useGoogleLogIn();
+  const { isDark } = useDarkModeHook();
 
   const modalCloser = () => {
-    dispatch(setModalVisible(false))
-  }
+    dispatch(setModalVisible(false));
+  };
 
   return (
-    <div className={isDark ? "login_modal dark" : "login_modal"}>
-      <div className={isDark ? "x_icon_login_modal dark" : "x_icon_login_modal"} onClick={modalCloser}>{xIcon}</div>
+    <>
+      <div className="login_backdrop" onClick={modalCloser}/>
+      <div className={isDark ? "login_modal dark" : "login_modal"}>
+        <div
+          className={isDark ? "x_icon_login_modal dark" : "x_icon_login_modal"}
+          onClick={modalCloser}
+        >
+          {xIcon}
+        </div>
         <div className="log_modal_block1">
-            <p>Log In</p>
-            <p>By continuing, you agree to our User Agreement and <br/> acknowledge that you understand the Privacy Policy.</p>
+          <p>Log In</p>
+          <p>
+            By continuing, you agree to our User Agreement and <br />{" "}
+            acknowledge that you understand the Privacy Policy.
+          </p>
         </div>
 
         <div className="google_login">
@@ -29,9 +39,9 @@ const Login = () => {
             Google
           </div>
         </div>
-        
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
