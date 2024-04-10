67eccc2c071db312d8458fd53f7ac00b2b31c7d7
Despo <tornike.despotashvili@gmail.com>
2024-04-10 14:21:35 +0400

    update add post component

--- src/components/CreatePageComps/AddPost.tsx
+++ src/components/CreatePageComps/AddPost.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef } from "react";
 import "./CreatePageStyles.css";
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
