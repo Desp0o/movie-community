82aa410fe754e5b3b91fa2c77809a57d6ec478df
Despo <tornike.despotashvili@gmail.com>
2024-04-10 11:02:35 +0400

    update page layout css

--- src/components/singlePostPage/AddComment.tsx
+++ src/components/singlePostPage/AddComment.tsx
@@ -1,13 +1,26 @@
 import { SetStateAction, useState } from 'react';
+import { useUserHook } from '../../hooks/useUserHook';
+import { useDispatch } from 'react-redux';
+import { setModalVisible } from '../../Redux/loginModalSlicer';
 
 const AddComment = () => {
   const [text, setText] = useState('');
+  const { user } = useUserHook()
+  const disptach = useDispatch()
 
   const handleChange = (event: { target: { value: SetStateAction<string>; style: { height: string; }; scrollHeight: any; }; }) => {
     setText(event.target.value);
     event.target.style.height = 'auto';
     event.target.style.height = `${event.target.scrollHeight}px`;
   };
+
+  const logInHandler = () => {
+    disptach(setModalVisible(true))
+  }
+
+  if(!user.name){
+    return <p>please <span onClick={logInHandler} style={{color: 'var(--reddit)', cursor:"pointer"}}>log in</span> to leave comment.</p>
+  }
 
   return (
     <div>
