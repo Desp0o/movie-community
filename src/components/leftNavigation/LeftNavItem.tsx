c4563d8f9dc8308f68b0e3143508d614acd5c6ea
Despo <tornike.despotashvili@gmail.com>
2024-04-13 01:51:48 +0400

    close left menu on item click

--- src/components/leftNavigation/LeftNavItem.tsx
+++ src/components/leftNavigation/LeftNavItem.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from "react";
 import { useDarkModeHook } from "../../hooks/useDarkModeHook";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setLeftMenuState } from "../../Redux/leftMenuSlicer";
 
 interface leftNavItemProps {
   icon: ReactNode;
@@ -8,11 +10,25 @@ interface leftNavItemProps {
   path: string;
 }
 
+interface RootState {
+  leftMenuStore:{
+    isLeftMenuOpen: boolean
+  }
+}
+
 const LeftNavItem: React.FC<leftNavItemProps> = ({ icon, title, path }) => {
   const { isDark } = useDarkModeHook();
+  const isLeftMenuOpen = useSelector((state:RootState) => state.leftMenuStore.isLeftMenuOpen)
+  const dispatch = useDispatch()
+
+  const closeNavOnItemClick = () => {
+    if(isLeftMenuOpen){
+      dispatch(setLeftMenuState(false))
+    }
+  }
 
   return (
-    <Link to={path}>
+    <Link to={path} onClick={closeNavOnItemClick}>
       <div className={isDark ? "left_nav_item dark" : "left_nav_item"}>
         <div className="left_nav_item_icon">{icon}</div>
         <p>{title}</p>
