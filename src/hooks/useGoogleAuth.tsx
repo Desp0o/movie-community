9f27935c726b1d5adec7cc8273d8af34d7bdec2c
Despo <tornike.despotashvili@gmail.com>
2024-04-12 15:54:35 +0400

    update google signin

--- src/hooks/useGoogleAuth.tsx
+++ src/hooks/useGoogleAuth.tsx
@@ -3,6 +3,7 @@ import { app } from "../components/firebase/firebaseConfig";
 import { useDispatch } from 'react-redux';
 import { setUser } from '../Redux/userSlicer';
 import { setModalVisible } from '../Redux/loginModalSlicer';
+import axios from 'axios';
 
 
 export const useGoogleLogIn = () => {
@@ -20,6 +21,25 @@ export const useGoogleLogIn = () => {
           const signIn = await signInWithPopup(auth, provider);
           disptach(setUser({name: signIn.user.displayName, avatar: signIn.user.photoURL, userID: signIn.user.uid}))
           disptach(setModalVisible(false))
+          console.log(signIn);
+
+          const avatarFromGoogle = signIn.user.photoURL
+          const formatedAvatar = avatarFromGoogle?.substring(0, avatarFromGoogle.length - 4)
+          
+          const formData = {
+            name: signIn.user.displayName,
+            avatar: formatedAvatar + "250",
+            google_id: signIn.user.uid,
+            email: signIn.user.email,
+          }
+
+           const res = await axios.post('https://api.pinky.ge/api/register', formData, {
+            headers:{
+              'Content-Type': 'application/json'
+            }
+          })
+          console.log(res.data);
+          
           return signIn;
         } catch (error) {
           console.error(error);
