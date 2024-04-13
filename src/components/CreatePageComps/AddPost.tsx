88664a536463909a5ce57d09babafc6dfa0a6540
Despo <tornike.despotashvili@gmail.com>
2024-04-13 10:16:34 +0400

    remove unused code

--- src/components/CreatePageComps/AddPost.tsx
+++ src/components/CreatePageComps/AddPost.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import "./CreatePageStyles.css";
 import SendPostBTN from "./SendPostBTN";
 import axios from "axios";
