4c66f1425c3e2f16804f1d4d7add8d35f2630893
Despo <tornike.despotashvili@gmail.com>
2024-04-15 01:06:10 +0400

    update post image path

--- src/components/singlePostComp/postImage.tsx
+++ src/components/singlePostComp/postImage.tsx
@@ -9,8 +9,8 @@ interface PostImageImage {
 const PostImage: React.FC<PostImageImage> = ({image, funName}) => {
   return (
     <div className='single_post_image_container' onClick={funName}>
-        <img src={image} alt='post cover' className='single_post_cover' />
-        <img src={image} alt='post image' className='single_post_img' />
+        <img src={`https://api.pinky.ge/storage/img/post/${image}.webp`} alt='post cover' className='single_post_cover' />
+        <img src={`https://api.pinky.ge/storage/img/post/${image}.webp`} alt='post image' className='single_post_img' />
     </div>
   )
 }
