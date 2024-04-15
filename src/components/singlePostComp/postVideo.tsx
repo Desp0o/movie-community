d6740cd928e8515cf85a18f26cb20965c1c50cdb
Despo <tornike.despotashvili@gmail.com>
2024-04-15 19:44:00 +0400

    update video player functions

--- src/components/singlePostComp/postVideo.tsx
+++ src/components/singlePostComp/postVideo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { LegacyRef, useEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player";
 
 interface PostVideoProps {
   image: string;
@@ -7,11 +8,69 @@ interface PostVideoProps {
 const PostVideo: React.FC<PostVideoProps> = ({ image }) => {
   const videoStoragePath = import.meta.env.VITE_VIDEO_PATH;
 
+  const [hasPlayed, setHasPlayed] = useState(false);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const [isControlShown, setControlShown] = useState(false)
+
+  const showControlers = () => {
+    setControlShown(true)
+  }
+
+  const hideControlers = () => {
+    setControlShown(false)
+  }
+
+  const videoRef: LegacyRef<any> | undefined = useRef()
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // If the element is intersecting the viewport
+        if (entry.isIntersecting) {
+          setIsIntersecting(true);
+          if (!hasPlayed) {
+            setHasPlayed(true);
+          }
+        } else {
+          setIsIntersecting(false);
+        }
+      },
+      {
+        threshold: 0.5 
+      }
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, [hasPlayed]);
+
   return (
-    <video width="100%" height="100%" style={{maxHeight: '350px'}} controls controlsList="nodownload" autoPlay={true}>
-      <source src={`${videoStoragePath}${image}`} type="video/mp4" />
-      Your browser does not support the video tag.
-    </video>
+    <div ref={videoRef} onMouseEnter={showControlers} onMouseLeave={hideControlers}>
+      <ReactPlayer
+        className='react-player'
+        url={`${videoStoragePath}${image}`}
+        playing={isIntersecting}
+        controls={isControlShown}
+        muted={true}
+        width='100%'
+        height='100%'
+        style={{maxHeight:"400px"}}
+        ref={videoRef}
+        config={{
+          file: {
+            attributes: {
+              controlsList: "nodownload"
+            }
+          }}}
+      />
+    </div>
   );
 };
 
