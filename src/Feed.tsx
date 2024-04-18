0b243ece1da953587a90d3c5f5d99b7f47c89adc
Despo <tornike.despotashvili@gmail.com>
2024-04-18 12:58:30 +0400

    add infinity scroll

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -138,6 +138,7 @@ const Feed = () => {
           </div>
         ))}
       </div>
+      {!hasNextPage && (<p style={{fontWeight:"900", color:"#BC53D9"}}>no more posts</p>)}
     </PageLayout>
   );
 };
