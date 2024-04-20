4e81a7c5e8d45976884f0b37bb9e440d7f2038e5
Despo <tornike.despotashvili@gmail.com>
2024-04-20 20:07:04 +0400

    reg inputs

--- src/components/login/RegisterForm.tsx
+++ src/components/login/RegisterForm.tsx
@@ -1,86 +1,149 @@
-import { useState } from "react"
-import LoginModalBtn from "./LoginModalBtn"
-import axios from "axios"
-import "./login.css"
+import { useState } from "react";
+import LoginModalBtn from "./LoginModalBtn";
+import axios from "axios";
+import "./login.css";
 import { setModalVisible } from "../../Redux/loginModalSlicer";
 import { setUser } from "../../Redux/userSlicer";
 import { useDispatch } from "react-redux";
+import InputComponent from "../inputComponent/InputComponent";
+import { eyeIcon } from "../../assets/svg/eyeIco";
 
 const loginPath = import.meta.env.VITE_LOGIN;
 const regPath = import.meta.env.VITE_REGISTER;
 
 const RegisterForm = () => {
-    const dispatch = useDispatch()
-    const [response, setResponse] = useState('')
-    const [_isLoading, setLoading] = useState(false)
-    const [isPwdEqual, setPwdEqual] = useState(false)
-    const [regInputs, setRegInputs] = useState({
-        name: '',
-        email: '',
-        password: '',
-        password_confirmation: ''
-    })
-    // const [rePassword, setRePassword] = useState('')
+  const dispatch = useDispatch();
+  const [response, setResponse] = useState("");
+  const [_isLoading, setLoading] = useState(false);
+  const [isPwdEqual, setPwdEqual] = useState(false);
+  const [regInputs, setRegInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+  // const [rePassword, setRePassword] = useState('')
+  const [showPwd, setShowPwd] = useState(false)
+  const [showRePwd, setReShowPwd] = useState(false)
 
-    const regUser = async () => {
 
-        if(regInputs.password_confirmation === regInputs.password && regInputs.password.length !== 0){
-            setLoading(true)
-            setPwdEqual(false)
-            try {
-                const res = await axios.post(regPath,regInputs,{
-                    headers:{
-                        'Content-Type': 'application/json'
-                    }
-                })
-    
-                setResponse(res.data.message)
+  const regUser = async () => {
+    if (
+      regInputs.password_confirmation === regInputs.password &&
+      regInputs.password.length !== 0
+    ) {
+      setLoading(true);
+      setPwdEqual(false);
+      try {
+        const res = await axios.post(regPath, regInputs, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-                if(res.data.message === 'User registered by Mail'){
-                    const res = await axios.post(loginPath, { email: regInputs.email, password: regInputs.password },{
-                        headers:{
-                            'Content-Type': 'application/json'
-                        }
-                    });
-                    
-                    localStorage.setItem('token', res.data.token)
-                    localStorage.setItem('token_death', res.data.token_death)
-                    localStorage.setItem('userID', res.data.user.id)
-                    dispatch(setModalVisible(false))
-                    dispatch(setUser({name: res.data.user.email, avatar: res.data.user.avatar, userID: res.data.user.id}))                    
-                }
-                
-            } catch (error) {
-                console.error(error);
-                
+        setResponse(res.data.message);
+
+        if (res.data.message === "User registered by Mail") {
+          const res = await axios.post(
+            loginPath,
+            { email: regInputs.email, password: regInputs.password },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
             }
-            finally{
-                setLoading(false)
-            }
-        }
+          );
 
-        if(regInputs.password_confirmation !== regInputs.password){
-            setPwdEqual(true)
-            return
+          localStorage.setItem("token", res.data.token);
+          localStorage.setItem("token_death", res.data.token_death);
+          localStorage.setItem("userID", res.data.user.id);
+          dispatch(setModalVisible(false));
+          dispatch(
+            setUser({
+              name: res.data.user.email,
+              avatar: res.data.user.avatar,
+              userID: res.data.user.id,
+            })
+          );
         }
-
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
     }
+
+    if (regInputs.password_confirmation !== regInputs.password) {
+      setPwdEqual(true);
+      return;
+    }
+  };
 
   return (
     <>
-        <p className="register_title">Register</p>
-        <form className="reg_form" onSubmit={regUser}> 
-            <input placeholder="Name" name='name' value={regInputs.name} type="text" className="form_inputs" onChange={(e)=>setRegInputs({ ...regInputs, name: e.target.value })} autoComplete="name"/>
-            <input placeholder="Email" name="email" value={regInputs.email} type="text" className="form_inputs" onChange={(e)=>setRegInputs({ ...regInputs, email: e.target.value })} autoComplete="email"/>
-            <input placeholder="Password" name="password" value={regInputs.password} type="password" className="form_inputs" onChange={(e)=>setRegInputs({ ...regInputs, password: e.target.value })} autoComplete="password"/>
-            <input placeholder="Repeat Password" name="re_password" value={regInputs.password_confirmation} type="password" className="form_inputs" onChange={(e)=>setRegInputs({...regInputs, password_confirmation: e.target.value})} autoComplete="password"/>
+      <form className="reg_form" onSubmit={regUser}>
+        
+        <InputComponent 
+            type="text" 
+            autoComplete="name"
+            placeholder="Name"
+            value={regInputs.name} 
+            isError={false} 
+            onChange={(e) => setRegInputs({ ...regInputs, name: e.target.value })} 
+        />
+      
+        <InputComponent 
+            type="text"
+            autoComplete="current-email"
+            placeholder="Email"
+            value={regInputs.email} 
+            isError={false} 
+            onChange={(e) =>setRegInputs({ ...regInputs, email: e.target.value })} 
+        />
 
-            <LoginModalBtn funName={regUser} title="Register"/>
-        </form>
-        {response ? <p className="reg_fomr_res_status_okey">{response}</p> : <></>}
-        {isPwdEqual ? <p className="password_do_not_match">Password do not match</p> : <></>}
+        <div className="pasword_input_container">
+            <span className="eye_icon" onClick={()=>setShowPwd(!showPwd)}>
+            {eyeIcon}
+            </span>
+            <InputComponent 
+                type={showPwd ? "text" : "password"} 
+                autoComplete={"current_password"} 
+                placeholder="Password" 
+                value={regInputs.password} 
+                isError={false} 
+                onChange={(e) =>setRegInputs({ ...regInputs, password: e.target.value })} 
+            />
+        </div>    
+
+        <div className="pasword_input_container">
+            <span className="eye_icon" onClick={()=>setReShowPwd(!showRePwd)}>
+            {eyeIcon}
+            </span>
+            <InputComponent 
+                type={showRePwd ? "text" : "password"} 
+                autoComplete={"current_password"} 
+                placeholder="Password" 
+                value={regInputs.password_confirmation} 
+                isError={false} 
+                onChange={(e) =>setRegInputs({ ...regInputs, password_confirmation: e.target.value })} 
+            />
+        </div>
+
+        <LoginModalBtn funName={regUser} title="Register" />
+      </form>
+
+      {response ? (
+        <p className="reg_fomr_res_status_okey">{response}</p>
+      ) : (
+        <></>
+      )}
+      {isPwdEqual ? (
+        <p className="password_do_not_match">Password do not match</p>
+      ) : (
+        <></>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
