f17a14333f0c7c8b287f2bddd7a2030676bec42f
Despo <tornike.despotashvili@gmail.com>
2024-04-24 14:16:48 +0400

    create data formater comp

--- src/components/singlePostComp/Author.tsx
+++ src/components/singlePostComp/Author.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./singlePostComp.css";
 import noAvatar from "../../assets/noAvatar.jpeg";
-import { useDarkModeHook } from "../../hooks/useDarkModeHook";
+import DateFormater from "../dateFormater/DateFormater";
 
 interface AuthorProps {
   avatar: string;
@@ -10,53 +10,7 @@ interface AuthorProps {
 }
 
 const Author: React.FC<AuthorProps> = ({ avatar, name, date }) => {
-  const [timeAgo, setTimeAgo] = useState('');
-  const { isDark } = useDarkModeHook()
 
-  useEffect(() => {
-    const updateTimeAgo = () => {
-      const seconds = Math.floor((new Date().getTime() - new Date(date).getTime()) / 1000);
-      
-      let interval
-      interval = Math.floor(seconds / 31536000);
-      if (interval >= 1) {
-        setTimeAgo(`${interval} years ago`);
-        return;
-      }
-
-      interval = Math.floor(seconds / 2592000);
-      if (interval >= 1) {
-        setTimeAgo(`${interval} months ago`);
-        return;
-      }
-
-      interval = Math.floor(seconds / 86400);
-      if (interval >= 1) {
-        setTimeAgo(`${interval} days ago`);
-        return;
-      }
-
-      interval = Math.floor(seconds / 3600);
-      if (interval >= 1) {
-        setTimeAgo(`${interval} hours ago`);
-        return;
-      }
-
-      interval = Math.floor(seconds / 60);
-      if (interval > 1) {
-        setTimeAgo(`${interval} minutes ago`);
-        return;
-      }
-      
-      setTimeAgo(`Just now`);
-    };
-
-    updateTimeAgo();
-
-    const intervalId = setInterval(updateTimeAgo, 60000);
-
-    return () => clearInterval(intervalId);
-  }, [date]);
 
   return (
     <div className="author">
@@ -73,7 +27,7 @@ const Author: React.FC<AuthorProps> = ({ avatar, name, date }) => {
         <span className="span_dot" />
       </div>
 
-      <p className={isDark ? "elapsed_time dark" : "elapsed_time"}>{timeAgo}</p>
+      <DateFormater date={date} />
     </div>
   );
 };
