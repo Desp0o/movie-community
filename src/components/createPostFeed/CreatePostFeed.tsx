78b58e78ca0e5abc80037aaa8039d55f5cb08611
Despo <tornike.despotashvili@gmail.com>
2024-04-26 13:01:36 +0400

    update

--- src/components/createPostFeed/CreatePostFeed.tsx
+++ src/components/createPostFeed/CreatePostFeed.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { addMediaIocn } from "../../assets/svg/addMediaIcon"
 import { pollIcon } from "../../assets/svg/poolIcon"
 import { quizFeedIocn } from "../../assets/svg/quizFeedIcon"
@@ -7,22 +8,42 @@ import PostStyle from "./PostStyle"
 
 
 const CreatePostFeed = () => {
-    const { user } = useUserHook()
+    const { user } = useUserHook();
+    const [inputValue, setInputValue] = useState('')
 
-  return (
-    <div className="CreatePostFeed">
-        <div className="user_avatar_input">
-            <img src={user.avatar} alt="user avatar" className="user_avatar_feed" />
-            <input type="text" className="feed_input" placeholder="Create post"/>
-        </div>
+    const handleChange = (event: { target: { style: { height: string }; scrollHeight: any , value: string} }) => {
+        event.target.style.height = "44px";
+        event.target.style.height = `${event.target.scrollHeight}px`;
         
-        <div className="choose_post_style">
-            <PostStyle styleName={"Photo/video"} styleIocn={addMediaIocn} />
-            <PostStyle styleName={"Quiz"} styleIocn={quizFeedIocn} />
-            <PostStyle styleName={"Poll"} styleIocn={pollIcon} />
+        setInputValue(event.target.value)
+    };
+
+    return (
+        <div className="CreatePostFeed">
+            <div className="user_avatar_input">
+                <img
+                    src={user.avatar}
+                    alt="user avatar"
+                    className="user_avatar_feed"
+                />
+
+                <textarea
+                    className="feed_input"
+                    placeholder="Create post"
+                    value={inputValue}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="choose_post_style">
+                <PostStyle styleName={"Photo/video"} styleIocn={addMediaIocn} />
+                <PostStyle styleName={"Quiz"} styleIocn={quizFeedIocn} />
+                <PostStyle styleName={"Poll"} styleIocn={pollIcon} />
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
+
+
 
 export default CreatePostFeed
--- src/components/createPostFeed/PostStyle.tsx
+++ src/components/createPostFeed/PostStyle.tsx
@@ -3,15 +3,16 @@ import React, { ReactElement } from 'react'
 interface PostStyleProps {
     styleName: string;
     styleIocn: ReactElement;
+    onclick?: () => void
 }
 
 const PostStyle:React.FC<PostStyleProps> = ({styleName, styleIocn}) => {
   return (
-    <div className='postStyle_container'>
+    <div className='postStyle_container' onClick={() => onclick}>
         <span>{styleIocn}</span>
         <p>{styleName}</p>
     </div>
   )
 }
 
-export default PostStyle
+export default PostStyle;
