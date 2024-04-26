d9cacf7359a5d190b8003f3b78e4a2235d2a8bb8
Despo <tornike.despotashvili@gmail.com>
2024-04-27 01:20:44 +0400

    create sned fucntion

--- src/components/createPostFeed/AddPostPopUp.tsx
+++ src/components/createPostFeed/AddPostPopUp.tsx
@@ -7,21 +7,108 @@ import PostBottomButtons from "./addPostComps/PostBottomButtons"
 import PostTextarea from "./addPostComps/PostTextarea"
 import { setAddPostModal } from "../../Redux/postModal"
 import { usePostAddModalHook } from "../../hooks/usePostAddModalHook"
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
+import { useUserHook } from "../../hooks/useUserHook"
+import axios from "axios"
+import { setRefetch } from "../../Redux/RefetchSlicer"
+import { useRefetchHook } from "../../hooks/useRefetchHook"
+import { toast } from "react-toastify"
 
 const AddPostPopUp = () => {
+  const notify = () => toast.success('Post Added Successfully !',{ autoClose: 1000, theme: "colored" });
+  const notifyError = () => toast.error('Error',{ autoClose: 1000, theme: "colored" });
   const dispatch = useDispatch()
+  const { user } = useUserHook()
+  const { requestRefetch } = useRefetchHook()
   const { addPostModalStates } = usePostAddModalHook()
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [uploadedImage, setUploadedImage] = useState<string>('')
+  const [postValue, setPostValue] = useState<{
+    img: File | undefined;
+    title: string;
+    text: string;
+    user_id: number | string;
+    type: number | null;
+  }>({
+    img: undefined,
+    title: "",
+    text: "",
+    user_id: user.userID,
+    type: null
+  });
 
 
+  
+
+  // close modal
   const closeDefaultPostAddModal = () => {
     dispatch(setAddPostModal({defaultPost: false, pollPost: false, quizPost: false, openImageUpload: false, showPostButtons: true}))
+    console.log(1);
+    
   }
 
   useEffect(()=>{
-console.log(addPostModalStates);
 
-  },[addPostModalStates])
+    if(postValue.img?.type.includes('video')){
+      setPostValue({ ...postValue, type: 1 });
+    }
+
+    if(postValue.img?.type.includes('image')){
+      setPostValue({ ...postValue, type: 0 });
+    }
+    //eslint-disable-next-line
+  },[postValue.img])
+
+  useEffect(()=>{
+    if(addPostModalStates.openImageUpload){
+      fileInputRef.current && fileInputRef.current.click(); //ინპუტზე დაკლიკების იმიტაცია
+      dispatch(setAddPostModal({showPostButtons: true, defaultPost: true}))
+    }
+     //გახდება ფოლსი ახლიდან დაკლიკება რო შევძლოთ ინფუთზე
+  },[addPostModalStates.openImageUpload])
+
+  //სურათის ასატვირთი ფუნქცია
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setUploadedImage(URL.createObjectURL(file)); //create image adress
+      dispatch(setAddPostModal({defaultPost: true, pollPost: false, showPostButtons: false})) 
+    }
+
+    setPostValue({ ...postValue, img: file });
+  };
+
+  const handleChange = (event: {
+    target: {
+      value: string;
+      style: { height: string };
+      scrollHeight: number;
+    };
+  }) => {
+    event.target.style.height = "120px";
+    event.target.style.height = `${event.target.scrollHeight}px`;
+    setPostValue({...postValue, title: event.target.value})
+  };
+
+  const sendPost = async () => {   
+    const token = localStorage.getItem('token')
+      try {
+          await axios.post(import.meta.env.VITE_POSTING , postValue, {
+            headers: {
+              Authorization:`Bearer ${token}`,
+              'Content-Type': 'multipart/form-data, application/json, text/plain, */*'
+            }
+          })
+          notify()
+          closeDefaultPostAddModal()
+          dispatch(setRefetch(!requestRefetch))
+          setUploadedImage('')
+          setPostValue({...postValue, text:'', title:'', img: undefined, type: 0})
+        } catch (error: unknown) {
+          notifyError()
+        }finally{
+      }
+  }
 
   return (
     
@@ -32,7 +119,12 @@ console.log(addPostModalStates);
             <CreateTitle title={"Create Post"} funcName={closeDefaultPostAddModal}/>
             <div className="post_body">
                 <PostAuthor />
-                <PostTextarea />
+                <PostTextarea 
+                  postValieProp={postValue.title} 
+                  handleChangeProp={handleChange} 
+                  uploadedImageProp={uploadedImage} 
+                  fileInputRefProp={fileInputRef} 
+                  handleFileChangeProp={handleFileChange} />
                 {addPostModalStates.showPostButtons && <PostBottomButtons /> }
             </div>
 
@@ -40,7 +132,7 @@ console.log(addPostModalStates);
               <LoginModalBtn 
                 title={"Post"} 
                 btnWidth="480px" 
-                funName={()=>{}} 
+                funName={sendPost} 
               />
             </div>
         </div>
--- src/components/createPostFeed/addPostComps/PostTextarea.tsx
+++ src/components/createPostFeed/addPostComps/PostTextarea.tsx
@@ -1,81 +1,51 @@
-import { useEffect, useRef, useState } from "react";
-import { smileIcon } from "../../../assets/svg/smileIcon"
-import { useUserHook } from "../../../hooks/useUserHook";
-import { usePostAddModalHook } from "../../../hooks/usePostAddModalHook";
-import { useDispatch } from "react-redux";
-import { setAddPostModal } from "../../../Redux/postModal";
+import React, { RefObject } from "react";
+import { smileIcon } from "../../../assets/svg/smileIcon";
 
-const PostTextarea = () => {
-  const dispatch = useDispatch()
-  const { user } = useUserHook()
-  const { addPostModalStates } = usePostAddModalHook()
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const [uploadedImage, setUploadedImage] = useState<string>('')
-  const [postValue, setPostValue] = useState<{
-    img: File | undefined;
-    title: string;
-    text: string;
-    user_id: number | string;
-    type: number | null;
-  }>({
-    img: undefined,
-    title: "",
-    text: "",
-    user_id: user.userID,
-    type: null
-  });
-
-  useEffect(()=>{
-
-    if(postValue.img?.type.includes('video')){
-      setPostValue({ ...postValue, type: 1 });
-    }
-
-    if(postValue.img?.type.includes('image')){
-      setPostValue({ ...postValue, type: 0 });
-    }
-    //eslint-disable-next-line
-  },[postValue.img])
-
-
-
-  useEffect(()=>{
-    if(addPostModalStates.openImageUpload){
-      fileInputRef.current && fileInputRef.current.click(); //ინპუტზე დაკლიკების იმიტაცია
-    }
-    dispatch(setAddPostModal({defaultPost: true, pollPost: false, showPostButtons: true})) //გახდება ფოლსი ახლიდან დაკლიკება რო შევძლოთ ინფუთზე
-  },[addPostModalStates.openImageUpload])
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      setUploadedImage(URL.createObjectURL(file));
-      dispatch(setAddPostModal({defaultPost: true, pollPost: false, showPostButtons: false})) 
-    }
-
-    setPostValue({ ...postValue, img: file });
-  };
-
-  return (
-    <div className="post_txtarea_smile">
-        <textarea className="popup_textarea" placeholder="Write post..." />
-        <span>{smileIcon}</span>
-
-        {
-          uploadedImage && 
-          <div className="uploaded_image_addPost_container">
-            <img src={uploadedImage} alt="uploaded post img" className="uploaded_image_addPost" />
-          </div>
-        }
-
-        <input
-          ref={fileInputRef}
-          type="file"
-          onChange={handleFileChange}
-          style={{ display: "none" }}
-        />
-    </div>
-  )
+interface PostTextareaProps {
+  postValieProp: string;
+  handleChangeProp: (event: {
+    target: { value: string; style: { height: string }; scrollHeight: number };
+  }) => void;
+  uploadedImageProp: string;
+  fileInputRefProp: RefObject<HTMLInputElement>;
+  handleFileChangeProp: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default PostTextarea
+const PostTextarea: React.FC<PostTextareaProps> = ({
+  postValieProp,
+  handleChangeProp,
+  uploadedImageProp,
+  fileInputRefProp,
+  handleFileChangeProp,
+}) => {
+  return (
+    <div className="post_txtarea_smile">
+      <textarea
+        className="popup_textarea"
+        placeholder="Write post..."
+        value={postValieProp}
+        onChange={handleChangeProp}
+      />
+      <span>{smileIcon}</span>
+
+      {uploadedImageProp && (
+        <div className="uploaded_image_addPost_container">
+          <img
+            src={uploadedImageProp}
+            alt="uploaded post img"
+            className="uploaded_image_addPost"
+          />
+        </div>
+      )}
+
+      <input
+        ref={fileInputRefProp}
+        type="file"
+        onChange={handleFileChangeProp}
+        style={{ display: "none" }}
+      />
+    </div>
+  );
+};
+
+export default PostTextarea;
