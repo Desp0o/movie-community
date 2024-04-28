bf8c4887ae658eedd7f2295658a034f6ca83d51f
Despo <tornike.despotashvili@gmail.com>
2024-04-29 02:27:15 +0400

    create and update poll post popup

--- src/components/createPostFeed/AddPoll.tsx
+++ src/components/createPostFeed/AddPoll.tsx
@@ -1,6 +1,5 @@
 import { useDispatch } from "react-redux";
 import { setAddPostModal } from "../../Redux/postModal";
-import BackDrop from "../backDrop/BackDrop";
 import LoginModalBtn from "../login/LoginModalBtn";
 import CreateTitle from "./addPostComps/CreateTitle";
 import { usePostAddModalHook } from "../../hooks/usePostAddModalHook";
@@ -8,12 +7,14 @@ import { useState } from "react";
 import { smileIcon } from "../../assets/svg/smileIcon";
 import axios from "axios";
 import { optionsAddIcon } from "../../assets/svg/optionsAddIcon";
+import InputComponent from "../inputComponent/InputComponent";
 
 const AddPoll = () => {
-    const token = localStorage.getItem("token");
-    const dispatch = useDispatch()
+  const token = localStorage.getItem("token");
+  const dispatch = useDispatch();
   const { addPostModalStates } = usePostAddModalHook();
-  const [pollQuestion, setPollQuestion] = useState('')
+  const [pollQuestion, setPollQuestion] = useState("");
+  const [pollOptions, setPollOptions] = useState(["", ""]);
 
   const closeDefaultPostAddModal = () => {
     dispatch(
@@ -27,68 +28,85 @@ const AddPoll = () => {
     );
   };
 
+  const addOption = () => {
+    setPollOptions([...pollOptions, ""]);
+  };
+
+  const updateOption = (index:number, value: any) => {
+    const newOptions = [...pollOptions];
+    newOptions[index] = value;
+    setPollOptions(newOptions);    
+  };
+
+
   const sendPoll = async () => {
     try {
-        const response =  await axios.post('', pollQuestion, {
-            headers:{
-                Authorization: `Bearer ${token}`
-            }
-        })
-        console.log(response.data);
-        
+      const response = await axios.post(
+        "/api/polls",
+        {
+          question: pollQuestion,
+          options: pollOptions,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response.data);
     } catch (error) {
-        console.error(error)
+      console.error(error);      
     }
-  }
+  };
 
-    return (
-        addPostModalStates.pollPost && (
-          <div style={{ width: "100%" }}>
-            <BackDrop funcName={closeDefaultPostAddModal} />
-            <div className="AddPostPopUp">
-              <CreateTitle
-                title={"Create poll"}
-                funcName={closeDefaultPostAddModal}
+  return (
+    addPostModalStates.pollPost && (
+      <div style={{ width: "100%" }}>
+        <div className="AddPostPopUp">
+          <CreateTitle title={"Create poll"} funcName={closeDefaultPostAddModal} />
+
+          <div className="quiz_quest_title">
+            <p className="your_question">Your question</p>
+            <div className="quiz_txtArea_container">
+              <span className="quizImgAddIcon">{smileIcon}</span>
+              <textarea
+                className="question_textarea"
+                onChange={(e) => setPollQuestion(e.target.value)}
               />
-    
-              <div className="quiz_quest_title">
-                <p className="your_question">Your question</p>
-                <div className="quiz_txtArea_container">
-                  <span className="quizImgAddIcon">
-                    {smileIcon}
-                  </span>
-                  <textarea
-                    className="question_textarea"
-                    onChange={(e) =>
-                        setPollQuestion(e.target.value)
-                    }
-                  />
-                </div>
-              </div>
+            </div>
+          </div>
 
-              <div className="quiz_answ_block">
+          <div className="quiz_answ_block">
             <p className="Correct_answer">Options</p>
 
-            <div className="options_poll">
-            <div className="add_option">
+            <div className="options_container">
+              {pollOptions.map((option, index) => (
+                <InputComponent
+                  key={index}
+                  type={"text"}
+                  autoComplete={""}
+                  placeholder={"Option " + (index + 1)}
+                  value={option}
+                  isError={false}
+                  widthProp="100%"
+                  onChange={(e) => updateOption(index, e.target.value)}
+                />
+              ))}
+            </div>
+
+              <div className="add_option" onClick={addOption}>
                 {optionsAddIcon}
                 <p>Add option</p>
-            </div>
-          </div>
-          </div>
-
-          
-
-    
-              <div className="quiz_btn_block">
-                <LoginModalBtn title={"Post"} funName={sendPoll} btnWidth="100%" />
               </div>
-    
-              
-            </div>
           </div>
-        )
-      );
-}
 
-export default AddPoll
+          <div className="quiz_btn_block">
+            <LoginModalBtn title={"Post"} funName={sendPoll} btnWidth="100%" />
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default AddPoll;
