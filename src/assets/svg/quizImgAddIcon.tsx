57f7801f05795b87e03cf961ba70ed73fd851c11
Despo <tornike.despotashvili@gmail.com>
2024-04-29 01:13:08 +0400

    update quiz add modal

---
+++ src/assets/svg/quizImgAddIcon.tsx
@@ -0,0 +1,5 @@
+export const quizImgAddIcon = <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+<path d="M12.5 0.5H1.5C0.947715 0.5 0.5 0.947715 0.5 1.5V12.5C0.5 13.0523 0.947715 13.5 1.5 13.5H12.5C13.0523 13.5 13.5 13.0523 13.5 12.5V1.5C13.5 0.947715 13.0523 0.5 12.5 0.5Z" stroke="#131518"/>
+<path d="M4.5 5.5C5.05228 5.5 5.5 5.05228 5.5 4.5C5.5 3.94772 5.05228 3.5 4.5 3.5C3.94772 3.5 3.5 3.94772 3.5 4.5C3.5 5.05228 3.94772 5.5 4.5 5.5Z" stroke="#131518"/>
+<path d="M3 11L4.5 8.5L6 11L8.5 7L11 11" stroke="#131518" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
--- src/components/createPostFeed/AddQuiz.tsx
+++ src/components/createPostFeed/AddQuiz.tsx
@@ -5,17 +5,23 @@ import { setAddPostModal } from "../../Redux/postModal";
 import BackDrop from "../backDrop/BackDrop";
 import InputComponent from "../inputComponent/InputComponent";
 import LoginModalBtn from "../login/LoginModalBtn";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import axios from "axios";
+import { quizImgAddIcon } from "../../assets/svg/quizImgAddIcon";
+import ReactPlayer from "react-player";
 
 const AddQuiz = () => {
-  const token = localStorage.getItem('token')
+  const token = localStorage.getItem("token");
   const { addPostModalStates } = usePostAddModalHook();
   const dispatch = useDispatch();
+  const quizFileInputRef = useRef<HTMLInputElement>(null);
+  const [uploadedImage, setUploadedImage] = useState("");
+  const [uploadedVideo, setUploadedVideo] = useState("");
   const [quizAnswers, setQuizAnswers] = useState({
-    geoAnswer: '',
-    engAnswer: ''
-  })
+    question: "",
+    geoAnswer: "",
+    engAnswer: "",
+  });
 
   const closeDefaultPostAddModal = () => {
     dispatch(
@@ -31,18 +37,37 @@ const AddQuiz = () => {
 
   const sendQuiz = async () => {
     try {
-        const response = await axios.post('', quizAnswers, {
-            headers:{
-                Authorization:`Bearer ${token}`,
-                "Content-Type":"multipart/form-data, application/json, text/plain, */*"
-            }
-        })
-        console.log(response?.data);
-        
+      const response = await axios.post("", quizAnswers, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type":
+            "multipart/form-data, application/json, text/plain, */*",
+        },
+      });
+      console.log(response?.data);
     } catch (error) {
-    console.error(error)
+      console.error(error);
     }
-  }
+  };
+
+  const openFileUploadWindow = () => {
+    quizFileInputRef.current?.click();
+  };
+
+  const handleQuizFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target?.files?.[0];
+    if (file) {
+      if (file.type.includes("image")) {
+        setUploadedImage(URL.createObjectURL(file));
+        setUploadedVideo("");
+      }
+
+      if (file.type.includes("video")) {
+        setUploadedVideo(URL.createObjectURL(file));
+        setUploadedImage("");
+      }
+    }
+  };
 
   return (
     addPostModalStates.quizPost && (
@@ -56,48 +81,91 @@ const AddQuiz = () => {
 
           <div className="quiz_quest_title">
             <p className="your_question">Your question</p>
-            <textarea className="question_textarea" />
+            <div className="quiz_txtArea_container">
+              <span className="quizImgAddIcon" onClick={openFileUploadWindow}>
+                {quizImgAddIcon}
+              </span>
+              <textarea
+                className="question_textarea"
+                onChange={(e) =>
+                  setQuizAnswers({ ...quizAnswers, question: e.target.value })
+                }
+              />
+            </div>
           </div>
 
-          <div className="quiz_answers">
-
-          </div>
+          <div className="quiz_answers"></div>
 
           <div className="quiz_answ_block">
             <p className="Correct_answer">Correct answer</p>
-            <p className="answer_desc">Please enter the correct answer in Georgian and English language</p>
+            <p className="answer_desc">
+              Please enter the correct answer in Georgian and English language
+            </p>
 
             <div className="quiz_answer_inputs">
-                <InputComponent 
-                    type={"text"} 
-                    autoComplete={""} 
-                    placeholder={"Georgian"} 
-                    value={quizAnswers.geoAnswer} 
-                    isError={false} 
-                    widthProp="100%"
-                    onChange={(e)=> setQuizAnswers({...quizAnswers, geoAnswer: e.target.value})}
-                />
+              <InputComponent
+                type={"text"}
+                autoComplete={""}
+                placeholder={"Georgian"}
+                value={quizAnswers.geoAnswer}
+                isError={false}
+                widthProp="100%"
+                onChange={(e) =>
+                  setQuizAnswers({ ...quizAnswers, geoAnswer: e.target.value })
+                }
+              />
 
-                <InputComponent 
-                    type={"text"} 
-                    autoComplete={""} 
-                    placeholder={"Eglish"} 
-                    value={quizAnswers.engAnswer} 
-                    isError={false} 
-                    widthProp="100%"
-                    onChange={(e)=> setQuizAnswers({...quizAnswers, engAnswer: e.target.value})}
+              <InputComponent
+                type={"text"}
+                autoComplete={""}
+                placeholder={"Eglish"}
+                value={quizAnswers.engAnswer}
+                isError={false}
+                widthProp="100%"
+                onChange={(e) =>
+                  setQuizAnswers({ ...quizAnswers, engAnswer: e.target.value })
+                }
+              />
+            </div>
+
+            <div className="uploaded_image_quiz_container">
+              {uploadedImage && (
+                <img
+                  src={uploadedImage}
+                  className="uploaded_image_addPost"
+                  alt="quiz img"
                 />
+              )}
+              {uploadedVideo && (
+                <ReactPlayer
+                  className="video_popupPorst_add"
+                  url={`${uploadedVideo}`}
+                  muted={true}
+                  width="100%"
+                  height="100%"
+                  config={{
+                    file: {
+                      attributes: {
+                        controlsList: "nodownload",
+                        playsInline: true,
+                      },
+                    },
+                  }}
+                />
+              )}
             </div>
           </div>
 
           <div className="quiz_btn_block">
-            <LoginModalBtn 
-                title={"Post"} 
-                funName={sendQuiz} 
-                btnWidth="100%"
-            />
+            <LoginModalBtn title={"Post"} funName={sendQuiz} btnWidth="100%" />
           </div>
-       
+
+          <input
+            type="file"
+            style={{ display: "none" }}
+            ref={quizFileInputRef}
+            onChange={handleQuizFileChange}
+          />
         </div>
       </div>
     )
