be78d2c3f22f1c23c99fea549faad5de9ecf8cac
tornike despotashvili <tornike.despotashvili@gmail.com>
2024-05-31 11:19:12 +0400

    changed bg

--- src/components/pageLayout/PageLayout.tsx
+++ src/components/pageLayout/PageLayout.tsx
@@ -10,6 +10,10 @@ const PageLayout:React.FC<PageLayout> = ({children}) => {
   return (
     <div className='page_layout'>
       <Navbar />
+      <div className='eclipse-bg-top'/>
+      <div className='eclipse-bg-right'/>
+      <div className='eclipse-bg-left'/>
+      <div className='eclipse-bg-bottom'/>
       <div className='page_layout_inner'>
       {children}
       </div>
