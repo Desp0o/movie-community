99ac375ee950005388c8ab302fa7dbddb8b82ec6
tornike despotashvili <tornike.despotashvili@gmail.com>
2024-06-06 01:08:48 +0400

    update replay comments and comments liking

--- src/components/commenting/CommentsSection.tsx
+++ src/components/commenting/CommentsSection.tsx
@@ -1,98 +1,142 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useUserHook } from "../../hooks/useUserHook";
-import noAvatar from "../../assets/noAvatar.jpeg"
-// import { deleteComment } from "./DELcomment";
-// import { editComment } from "./EDITcomment";
-import Author from "../singlePostComp/Author";
-import { heartIcon } from "../../assets/svg/heartIcon";
-import { dotsIcons } from "../../assets/svg/dotsIcons";
+import noAvatar from "../../assets/noAvatar.jpeg";
 import { deleteComment } from "./DELcomment";
 import axios from "axios";
 import ReplayComment from "./ReplayComment";
+import Author from "../singlePostComp/Author";
+import { heartIcon, activeHeartIcon } from "../../assets/svg/heartIcon";
+import { dotsIcons } from "../../assets/svg/dotsIcons";
 
 interface Comment {
   [x: string]: any;
   id: number;
   text: string;
   img: string;
-  user_id:string
+  user_id: string;
 }
 
 interface ComProps {
   fetchedComments: Comment[];
-  callback:()=>void
+  callback: () => void;
+}
+
+interface ReplayProps {
+  text: string;
 }
 
 // callback
-const CommentsSection: React.FC<ComProps> = ({ fetchedComments, callback  }) => {
-  // const imageStoragePath = import.meta.env.VITE_COMMENT_IMAGE;
-  const {user} = useUserHook()
+const CommentsSection: React.FC<ComProps> = ({ fetchedComments, callback }) => {
+  const { user } = useUserHook();
 
-  const [isSettingVisible, setSettingVisible] = useState(false)
+  const [isSettingVisible, setSettingVisible] = useState(false);
   const [clickedIndex, setClickedIndex] = useState<number | null>(null);
+  const [likedComments, setLikedComments] = useState<{ [key: number]: boolean }>({});
 
   const handleSetting = () => {
-    setSettingVisible(!isSettingVisible)
-  }
-  
+    setSettingVisible(!isSettingVisible);
+  };
 
-  const replay = (index:number | null)=>{
-    if(index === clickedIndex){
-      setClickedIndex(null)
-
-    }else{
-      setClickedIndex(index)
-
+  const replay = (index: number | null) => {
+    if (index === clickedIndex) {
+      setClickedIndex(null);
+    } else {
+      setClickedIndex(index);
     }
-  }
+  };
 
-  const likeComment = async (id: number) => {
-    const token = localStorage.getItem('token')
+  //set myCommGul on component load
+  useEffect(()=>{
+    fetchedComments.map((item, index)=>{
+      return(
+        setLikedComments((prev) => ({...prev, [index]: item.myCommGul !== 0 ? true : false}))
+      )
+    })
+  },[])
 
-      try {
-        const response = await axios.get(`${import.meta.env.VITE_COMMENT_LIKING}${id}`,{
-          headers:{
-            Authorization: `Bearer ${token}`
-          }
-        })
-        console.log(id);
-        
-        console.log(response.data);
-        
-      } catch (error) {
-        console.error(error)
-      }
-  }
+  //send comment like function
+  const likeComment = async (id: number, index: number) => {
+    const token = localStorage.getItem("token");
+
+    // update state on click
+    setLikedComments((prev) => ({ ...prev, [index]: !prev[index] }));
+
+    if (!token) {
+      console.error("No token found in localStorage");
+      return;
+    }
+
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_COMMENT_LIKING}${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error liking comment:", error);
+    } finally {
+      callback();
+    }
+  };
 
   return (
-    <div style={{display:"flex" , flexDirection:"column", gap:"30px"}}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "30px" }}>
       {fetchedComments?.map((item, index) => (
         <div key={item.id} className="single_posted_comment">
           {/* avatar */}
-          <img src={item.user.avatar ? item?.user?.avatar : noAvatar} alt="user vatar" className="comment_user_avatar" />
-          
+          <img
+            src={item.user.avatar ? item?.user?.avatar : noAvatar}
+            alt="user avatar"
+            className="comment_user_avatar"
+          />
+
           {/* name comment like replay */}
           <div className="comment_authorname_time">
             <Author name={item.user.name} date={item.created_at} />
             <p className="comment_text">{item.text}</p>
 
-            {Number(user.userID) === Number(item.user_id) && <span onClick={handleSetting} className="comment_dots">{dotsIcons}</span>}
-            
-            {
-              isSettingVisible && 
+            {Number(user.userID) === Number(item.user_id) && (
+              <span onClick={handleSetting} className="comment_dots">
+                {dotsIcons}
+              </span>
+            )}
+
+            {isSettingVisible && (
               <div className="post_setting_pannel">
-                <p onClick={() => deleteComment(item.id, callback)}> delete </p>
+                <p onClick={() => deleteComment(item.id, callback)}>delete</p>
               </div>
-            }
+            )}
 
             {/* comment likes */}
             <div className="like_comment_">
-                <span className="comment_heart_icon" onClick={()=>likeComment(item.id)}>{heartIcon}</span>
-                <span style={{height:"14px", borderRight:"1px solid #CCD4DE"}}/>
-                <p className="replay" onClick={()=>replay(index)}>Replay</p>
-                {index === clickedIndex ? <ReplayComment commentID={item.id}/> : <></>}
+              <span className="comment_heart_icon" onClick={() => likeComment(item.id, index)}>
+                {likedComments[index] ? activeHeartIcon : heartIcon}
+              </span>
+              <span style={{ height: "14px", borderRight: "1px solid #CCD4DE" }} />
+              <p className="replay" onClick={() => replay(index)}>
+                Replay
+              </p>
+              {index === clickedIndex ? (
+                <ReplayComment refetchCallback={callback} setter={setClickedIndex} commentID={item.id} feedID={item.feed_id} />
+              ) : null}
+              
+             
             </div>
-
+            
+            {/* replayed comments div */}
+            {
+              item.comments &&
+                <div className="replayed_comments">
+                    {item.comments?.map((replay: ReplayProps) => {
+                      return(
+                        <p>{replay.text}</p>
+                      )
+                    })}              
+                </div>
+            }
+            
           </div>
         </div>
       ))}
--- src/components/commenting/ReplayComment.tsx
+++ src/components/commenting/ReplayComment.tsx
@@ -1,31 +1,58 @@
 import axios from 'axios';
-import React from 'react'
+import React, { SetStateAction, useState } from 'react'
+import { Dispatch } from 'react';
+import { useUserHook } from '../../hooks/useUserHook';
 
 interface ReplayCommentProps {
     commentID: number;
+    feedID: number;
+    setter: Dispatch<SetStateAction<number | null>>;
+    refetchCallback: ()=>void
 }
 
-const ReplayComment: React.FC<ReplayCommentProps> = ({commentID}) => {
+const ReplayComment: React.FC<ReplayCommentProps> = ({commentID, feedID, setter, refetchCallback}) => {
+    const { user } = useUserHook();
+    const [replayValue, setReplayValue] = useState('')
 
-  const sendReplay = async () => {
+  const sendReplay = async (id: number) => {
     const token = localStorage.getItem('token')
 
     try {
-        const res = await axios.post('','',{
+        const res = await axios.post(`${import.meta.env.VITE_ADD_COMMENT}${feedID}`,{
+            text: replayValue,
+            img: null,
+            comment_id: id
+        },{
             headers:{
                 Authorization: `Bearer ${token}`
             }
         })
 
         console.log(res);
-        
+        setter(null)
     } catch (error) {
         console.error(error)
+    }finally{
+        refetchCallback()
     }
   }
 
   return (
-    <div>ReplayComment</div>
+    <div className="comment_container">
+      {user.avatar && <img src={user?.avatar} alt="user avatar" className="comment_user_avatar"/>}
+      <textarea
+        className="comment_textarea"
+        value={replayValue}
+        onChange={(e) => setReplayValue(e.target.value)}
+        placeholder="Add comment..."
+        style={{width:"250px"}}
+      />
+
+      {/* <input multiple type="file" onChange={handleFileChange} /> */}
+      <div className="comment_ntm" onClick={()=>sendReplay(commentID)}>
+        <p>Post</p>
+      </div>
+    </div>
   )
 }
 
