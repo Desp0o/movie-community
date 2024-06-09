75b091cf405ef6c2ed0009588da1f5620a6a3637
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-09 20:14:19 +0400

    quiz info left side

--- src/pages/Quiz_.tsx
+++ src/pages/Quiz_.tsx
@@ -1,7 +1,32 @@
+import { QUIZ } from "../assets/newSvg/QUIZ"
+import "./Quiz_.css"
+
 
 const Quiz_ = () => {
   return (
-    <div>Quiz_</div>
+    <div className="Quiz_">
+
+      <div className="quiz_add_rules">
+
+        {/* title */}
+        <div className="quiz_add_rules_title">
+          <p>Create a </p>
+          {QUIZ}
+        </div>
+
+        {/* text */}
+        <p className="quiz_add_rules_text">
+        Welcome to the ... Quiz Creator! Here are the only rules you need to follow:<br/><br/>
+
+All questions must be about movies.<br/>
+Each question should have four possible answers, but only one of them is the right one.<br/>
+Feel free to add photos to your questions, or skip them entirely. It’s your call!<br/><br/>
+
+Get creative and have fun crafting your ultimate movie quiz!
+        </p>
+      </div>
+
+    </div>
   )
 }
 
