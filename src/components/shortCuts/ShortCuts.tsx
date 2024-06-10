9b4e33477222e56e143fa7f976f6bae18a311d58
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-10 12:36:10 +0400

    update

--- src/components/shortCuts/ShortCuts.tsx
+++ src/components/shortCuts/ShortCuts.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { pollShortCut } from "../../assets/newSvg/pollShortCut"
 import { quizShortCut } from "../../assets/newSvg/quizShortCut"
 import "./ShortCuts.css"
@@ -6,7 +7,9 @@ import SingleShortCut from "./SingleShortCut"
 const ShortCuts = () => {
   return (
     <div className="shortcuts">
+      <Link to="/pages/CreateQuiz">
         <SingleShortCut icon={quizShortCut} text={"Create quiz"} />
+      </Link>
         <SingleShortCut icon={pollShortCut} text={"Create poll"} />
     </div>
   )
--- src/pages/Quiz_.tsx
+++ src/pages/Quiz_.tsx
@@ -16,58 +16,58 @@ const Quiz_ = () => {
     <div className="Quiz_">
 
       <div className="Quiz_inner">
-      <div className="quiz_add_rules">
+        <div className="quiz_add_rules">
 
-{/* title */}
-<div className="quiz_add_rules_title">
-  <p>Create a </p>
-  {QUIZ}
-</div>
+          {/* title */}
+          <div className="quiz_add_rules_title">
+            <p>Create a </p>
+            {QUIZ}
+          </div>
 
-{/* text */}
-<p className="quiz_add_rules_text">
-  Welcome to the ... Quiz Creator! Here are the only rules you need to follow:<br /><br />
+          {/* text */}
+          <p className="quiz_add_rules_text">
+            Welcome to the ... Quiz Creator! Here are the only rules you need to follow:<br /><br />
 
-  All questions must be about movies.<br />
-  Each question should have four possible answers, but only one of them is the right one.<br />
-  Feel free to add photos to your questions, or skip them entirely. It’s your call!<br /><br />
+            All questions must be about movies.<br />
+            Each question should have four possible answers, but only one of them is the right one.<br />
+            Feel free to add photos to your questions, or skip them entirely. It’s your call!<br /><br />
 
-  Get creative and have fun crafting your ultimate movie quiz!
-</p>
+            Get creative and have fun crafting your ultimate movie quiz!
+          </p>
 
-{/* buttons */}
-<div className="Quiz_btns">
-  <ButtonFilled text={"Get started!"} link={""} />
-  <ButtonOutlined text={"Check out Quizzes"} link={""} />
-</div>
-</div>
+          {/* buttons */}
+          <div className="Quiz_btns">
+            <ButtonFilled text={"Get started!"} link={""} />
+            <ButtonOutlined text={"Check out Quizzes"} link={""} />
+          </div>
+        </div>
 
-<div className="guess_info_cards">
+        <div className="guess_info_cards">
 
-<div className="gc1">
-  <QUIZ_card image={matrix}>
-    <p className="matrix-txt">Who is the main character</p>
-    {inTheMovie}
-  </QUIZ_card>
-</div>
+          <div className="gc1">
+            <QUIZ_card image={matrix}>
+              <p className="matrix-txt">Who is the main character</p>
+              {inTheMovie}
+            </QUIZ_card>
+          </div>
 
-<div className="gc2">
-  <QUIZ_card image={bacToFuture}>
-    <p className="matrix-txt">Who is the main character</p>
-    {canYouName}
-  </QUIZ_card>
-</div>
+          <div className="gc2">
+            <QUIZ_card image={bacToFuture}>
+              <p className="matrix-txt">Who is the main character</p>
+              {canYouName}
+            </QUIZ_card>
+          </div>
 
-<div className="gc3">
-  <QUIZ_card image={et}>
-    <p className="matrix-txt">Who is the main character</p>
-    {canYouName}
-  </QUIZ_card>
-</div>
-</div>
+          <div className="gc3">
+            <QUIZ_card image={et}>
+              <p className="matrix-txt">Who is the main character</p>
+              {canYouName}
+            </QUIZ_card>
+          </div>
+        </div>
       </div>
 
-    <Footer />
+      <Footer />
     </div>
 
 
