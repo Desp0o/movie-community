9f843308f179b39460c27a8a5d007f225f777775
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-12 15:25:35 +0400

    updated footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,49 @@
+import { Link } from "react-router-dom"
+import { facebookIcon, instagramIcon } from "../../assets/newSvg/socialIcons"
 import "./footer.css"
 
 const Footer = () => {
   return (
     <div className='footer'>
+      {/* left side */}
+      <div className="footer_l">
+        <p className="Get_In_Touch">Get In Touch</p>
+        <p className="footer_p2">Contact us for any inquiries or collaborations regarding our movie community.</p>
 
+        <div className="social_icons_footer">
+          <a href="/" target="blank" rel="noopener noreferrer">{facebookIcon}</a>
+          <a href="/" target="blank" rel="noopener noreferrer">{instagramIcon}</a>
+        </div>
+      </div>
+
+      {/* right side */}
+      <div className="footer_r">
+
+        {/* feature block */}
+        <div className="features_block">
+          <p className="features_block_title">FEATURES</p>
+
+          {/* features */}
+          <div className="footer_r_features">
+            <Link to="" className="footer_r_item">Latest Polls</Link>
+            <Link to="" className="footer_r_item">Movie Filters</Link>
+            <Link to="" className="footer_r_item">Post Reviews</Link>
+            <Link to="" className="footer_r_item">Latest News</Link>
+            <Link to="" className="footer_r_item">Fun Quizzes</Link>
+            <Link to="" className="footer_r_item">Community</Link>
+          </div>
+        </div>
+
+        {/* contact */}
+        <div className="contact_block">
+            <p className="features_block_title">Contact</p>
+            <div className="contact_block_items">
+              <Link to="" className="footer_r_item">About  us</Link>
+              <Link to="" className="footer_r_item">Email</Link>
+              <Link to="" className="footer_r_item">Support</Link>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
