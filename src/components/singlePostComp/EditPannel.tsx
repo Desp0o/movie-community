b2428eddade84e0925f78e0ccd86355026069090
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-14 18:07:12 +0400

    udpate feed post setting pannel

--- src/components/singlePostComp/EditPannel.tsx
+++ src/components/singlePostComp/EditPannel.tsx
@@ -1,99 +1,99 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { canIcon } from '../../assets/svg/canIcon'
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
-import { penIcon } from '../../assets/svg/penIcon';
 import { setSpinnerState } from '../../Redux/spinnerSlicer';
 import PostEditPopUp from '../createPostFeed/PostEditPopUp';
 import { setFeedRefetch } from '../../Redux/feedRefetchSlicer';
 import { useRefetchHook } from '../../hooks/useFeedRefetch';
+import "../../pages/CreateQuiz.css"
+import { closeSquareIcon } from '../../assets/newSvg/closeSquareIcon';
 
-interface EditPannelPros{
-    postID: string | number;
-    isInnerPage?: boolean;
-    type: number | string;
+interface EditPannelPros {
+  postID: string | number;
+  isInnerPage?: boolean;
+  type: number | string;
 }
 
-const EditPannel:React.FC<EditPannelPros> = ({postID,isInnerPage, type}) => {
-    const notify = () => toast.success('Post deleted Successfully !',{ autoClose: 1000, theme: "colored" });
-    const notifyError = () => toast.error('Error',{ autoClose: 1000, theme: "colored" });
-    const token = localStorage.getItem('token')
-    const [isActive, setActive] = useState(false)
-    const { useFeedRefetch } = useRefetchHook()
-    const [openEditModal, setOpenEditModal] = useState(false)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    
-    const editPanelRef = useRef<HTMLDivElement>(null)
-    useEffect(() => {
-        const handleClick = (event: MouseEvent) => {
-            if (
-                editPanelRef.current &&
-                event.target instanceof Node &&
-                !editPanelRef.current.contains(event.target)
-            ) {
-                setActive(false);
-            }
-        };
-    
-        document.addEventListener("click", handleClick);
-    
-        return () => {
-            document.removeEventListener("click", handleClick);
-        };
-    }, [isActive]);
+const EditPannel: React.FC<EditPannelPros> = ({ postID, isInnerPage, type }) => {
+  const notify = () => toast.success('Post deleted Successfully !', { autoClose: 1000, theme: "colored" });
+  const notifyError = () => toast.error('Error', { autoClose: 1000, theme: "colored" });
+  const token = localStorage.getItem('token')
+  const [isActive, setActive] = useState(false)
+  const { useFeedRefetch } = useRefetchHook()
+  const [openEditModal, setOpenEditModal] = useState(false)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const handlePannel = () => {
-        setActive(!isActive)
-    }
+  const editPanelRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (
+        editPanelRef.current &&
+        event.target instanceof Node &&
+        !editPanelRef.current.contains(event.target)
+      ) {
+        setActive(false);
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [isActive]);
+
+  const handlePannel = () => {
+    setActive(!isActive)
+  }
 
 
-    const deletePost = async () => {
-      dispatch(setSpinnerState(true))
-        try {
-           await axios.get(`${import.meta.env.VITE_POST_DELETE}${postID}`,
-            {
-              headers:{
-                Authorization: `Bearer ${token}`
-              }
-            }
-          )
-          
-          notify()
-          if(isInnerPage){
-            navigate('/')
+  const deletePost = async () => {
+    dispatch(setSpinnerState(true))
+    try {
+      await axios.get(`${import.meta.env.VITE_POST_DELETE}${postID}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
           }
-          dispatch(setFeedRefetch(!useFeedRefetch))
-        } catch (error) {
-          console.error(error);
-          notifyError()
-        }finally{
-          dispatch(setSpinnerState(false))
         }
-    }
+      )
 
-    
-
-    const openEditPostModal = () => {
-      setOpenEditModal(true)
+      notify()
+      if (isInnerPage) {
+        navigate('/')
+      }
+      dispatch(setFeedRefetch(!useFeedRefetch))
+    } catch (error) {
+      console.error(error);
+      notifyError()
+    } finally {
+      dispatch(setSpinnerState(false))
     }
+  }
 
-    //prop function for edit modal close
-    const closeEditPostModal = () => {
-      setOpenEditModal(false)
-    }
+
+
+  const openEditPostModal = () => {
+    setOpenEditModal(true)
+  }
+
+  //prop function for edit modal close
+  const closeEditPostModal = () => {
+    setOpenEditModal(false)
+  }
 
   return (
     <>
-    {openEditModal && 
-      <PostEditPopUp 
-        postID={postID}
-        closeEditPostModal={closeEditPostModal}
-      />
-    }
-    <div ref={editPanelRef}>
+      {openEditModal &&
+        <PostEditPopUp
+          postID={postID}
+          closeEditPostModal={closeEditPostModal}
+        />
+      }
+      <div ref={editPanelRef}>
         <div className='pannel_dots' onClick={handlePannel}>
           <span className='panel_single_dot' />
           <span className='panel_single_dot' />
@@ -101,26 +101,19 @@ const EditPannel:React.FC<EditPannelPros> = ({postID,isInnerPage, type}) => {
         </div>
 
         {
-            isActive 
+          isActive
             ?
-            <div className="post_setting_pannel">
-              <div onClick={deletePost} className='post_delete'>
-                <span style={{width:"20px", height:"20px"}}>{canIcon}</span>
-                 Delete
-                 </div>
+            <div className="cr_quiz_card_editPanel">
 
-                 {
-                  type !==4 &&
-                  <div onClick={openEditPostModal} className='edit_delete'>
-                <span style={{width:"20px", height:"20px"}}>{penIcon}</span>
-                 Edit post
-                 </div>
-                }
+              <span onClick={() => setActive(false)}>{closeSquareIcon}</span>
+
+              {type === 0 || type === 1 && <p className='cr_quiz_card_editPanel_p' onClick={openEditPostModal}>edit post</p>}
+              <p className='cr_quiz_card_editPanel_p' onClick={deletePost}>delete post</p>
             </div>
             :
             <></>
         }
-    </div>
+      </div>
     </>
   )
 }
