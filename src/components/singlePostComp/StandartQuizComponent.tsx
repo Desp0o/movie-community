bdfb39fe9af517760acd8f2dc41cd9cb1f199d9d
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-15 19:12:41 +0400

    update quiz component

--- src/components/singlePostComp/StandartQuizComponent.tsx
+++ src/components/singlePostComp/StandartQuizComponent.tsx
@@ -16,46 +16,54 @@ interface StandartQuizComponentProps {
   authorName: string;
   date: string;
   postTitle: string;
-  quiz_id:number;
+  quiz_id: number;
 }
 
-const StandartQuizComponent:React.FC<StandartQuizComponentProps> = ({postUserId, image, postID, authorName, date, postTitle, quiz_id}) => {
+const StandartQuizComponent: React.FC<StandartQuizComponentProps> = ({ postUserId, image, postID, authorName, date, postTitle, quiz_id }) => {
   const { user } = useUserHook()
   const [isUserLogged, setUserLoged] = useState(false)
 
 
-  useEffect(()=>{
-    if(Number(user.userID) === postUserId){
+  useEffect(() => {
+    if (Number(user.userID) === postUserId) {
       setUserLoged(true)
-    }else{
+    } else {
       setUserLoged(false)
     }
-  },[user, isUserLogged, postUserId])
+  }, [user, isUserLogged, postUserId])
 
 
   return (
     <div className='StandartQuizComponent'>
-                  {isUserLogged ? <EditPannel postID={postID} type={4}/> : <></>}
+      {isUserLogged ? <EditPannel postID={postID} type={4} /> : <></>}
 
-    <div className="quiz_cover_container">
-       <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_bg"/>`
-      <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_img"/>`
-    </div>
+      <div className="quiz_cover_container">
+        {
+          image
+            ? <>
+              <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_bg" />
+              <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_img" />
+            </>
+            : <img src={quizDefaultCover} alt="quiz cover" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
 
-    <div className="quiz_details">
-      <Author name={authorName} date={date} />
-      <p className="quiz_title">{postTitle}</p>
-    </div>
+        }
 
-    <Link to={`/pages/Quiz/${quiz_id}`} className="quiz_btn_link">
-      <div className="satrt_quiz_btn">
-        <p>Get started!</p>
       </div>
-    </Link>
 
-    <span className="quiz_popcorn_icon">{popcornIcon}</span>
-    <span className="quiz_galss_icon">{glassIcon}</span>
-    
+      <div className="quiz_details">
+        <Author name={authorName} date={date} />
+        <p className="quiz_title">{postTitle}</p>
+      </div>
+
+      <Link to={`/pages/Quiz/${quiz_id}`} className="quiz_btn_link">
+        <div className="satrt_quiz_btn">
+          <p>Get started!</p>
+        </div>
+      </Link>
+
+      <span className="quiz_popcorn_icon">{popcornIcon}</span>
+      <span className="quiz_galss_icon">{glassIcon}</span>
+
     </div>
   )
 }
