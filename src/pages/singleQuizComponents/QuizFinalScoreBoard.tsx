601e621ebc3ad2e378408bf0cb00d1e1b8766bd7
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-25 11:10:03 +0400

    udate quiz final card

--- src/pages/singleQuizComponents/QuizFinalScoreBoard.tsx
+++ src/pages/singleQuizComponents/QuizFinalScoreBoard.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { quizNeon } from '../../assets/newSvg/quizNeon'
-import { starIcon } from '../../assets/newSvg/starIcon'
-import ButtonOutlined from '../../components/buttonFIlled/ButtonOutlined'
-import ButtonFIlled from '../../components/buttonFIlled/ButtonFilled'
-import Author from '../../components/singlePostComp/Author'
-import { useUserHook } from '../../hooks/useUserHook'
-import { congratulateIcon } from '../../assets/newSvg/congratulateIcon'
+import React from 'react';
+import { quizNeon } from '../../assets/newSvg/quizNeon';
+import { starIcon } from '../../assets/newSvg/starIcon';
+import ButtonOutlined from '../../components/buttonFIlled/ButtonOutlined';
+import ButtonFIlled from '../../components/buttonFIlled/ButtonFilled';
+import Author from '../../components/singlePostComp/Author';
+import { useUserHook } from '../../hooks/useUserHook';
+import { congratulateIcon } from '../../assets/newSvg/congratulateIcon';
+import { smile } from '../../assets/newSvg/smile';
+import { crossedFingers } from '../../assets/newSvg/crossedFingers';
+import { useLanguage } from '../../hooks/useLanguage';
 
 interface QuizFinalScoreBoardProps {
     title: string;
@@ -14,8 +17,9 @@ interface QuizFinalScoreBoardProps {
 }
 
 const QuizFinalScoreBoard: React.FC<QuizFinalScoreBoardProps> = ({ title, questionSum, correctAnswers }) => {
-
-    const { user } = useUserHook()
+    const { user } = useUserHook();
+    const { selectedLanguage } = useLanguage()
+    const percentage = (correctAnswers / questionSum) * 100;
 
     return (
         <>
@@ -26,17 +30,17 @@ const QuizFinalScoreBoard: React.FC<QuizFinalScoreBoardProps> = ({ title, questi
 
             <div className='quiz_final_scores'>
                 <div className='congrats'>
-                    <div className='congrats_block1'>
-                        <p className='congrats'>Congratulations</p>
-                        {congratulateIcon}
-                    </div>
+                    {percentage >= 70 ? <Congratulate /> : null}
+                    {percentage >= 50 && percentage < 70 ? <DidWell /> : null}
+                    {percentage < 50 ? <BetterLuckNextTime /> : null}
+
                     <Author name={user.name} avatar={user.avatar} />
                 </div>
 
                 <div className='quiz_final_stat_txts'>
-                    <p className='Correct_Answers'>Correct Answers: <span>{correctAnswers}/{questionSum}</span></p>
+                    <p className='Correct_Answers'>{selectedLanguage.quizGame.Correct_Answers}<span>{correctAnswers}/{questionSum}</span></p>
                     <p className='Points_earned'>
-                        Points earned: <span>{correctAnswers * 2}</span>
+                        {selectedLanguage.quizGame.Points_earned} <span>{correctAnswers * 2}</span>
                         {starIcon}
                     </p>
                 </div>
@@ -47,7 +51,37 @@ const QuizFinalScoreBoard: React.FC<QuizFinalScoreBoardProps> = ({ title, questi
                 <ButtonFIlled text={'Check out other quizzes'} link={'/pages/Quizzes'} wProp="215px" />
             </div>
         </>
-    )
+    );
 }
 
-export default QuizFinalScoreBoard
+export default QuizFinalScoreBoard;
+
+const Congratulate = () => {
+    const { selectedLanguage } = useLanguage()
+    return (
+        <div className='congrats_block1'>
+            <p className='congrats'>{selectedLanguage.quizGame.Congratulations}</p>
+            {congratulateIcon}
+        </div>
+    );
+}
+
+const DidWell = () => {
+    const { selectedLanguage } = useLanguage()
+    return (
+        <div className='congrats_block1'>
+            <p className='congrats'>{selectedLanguage.quizGame.You_did_well}</p>
+            {smile}
+        </div>
+    );
+}
+
+const BetterLuckNextTime = () => {
+    const { selectedLanguage } = useLanguage()
+    return (
+        <div className='congrats_block1'>
+            <p className='congrats'>{selectedLanguage.quizGame.Better_luck_next_time}</p>
+            {crossedFingers}
+        </div>
+    );
+}
