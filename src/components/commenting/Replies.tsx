f72f716e82cccb0970c62328c607f5b8408f4452
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-06-26 14:17:25 +0400

    update replies with setting dots

--- src/components/commenting/Replies.tsx
+++ src/components/commenting/Replies.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { dotsForComments } from '../../assets/newSvg/dotsForComments';
+import { useUserHook } from '../../hooks/useUserHook';
 
 interface RepliesProps {
     replayedComments: []
@@ -6,16 +8,26 @@ interface RepliesProps {
 
 interface itemProps {
     text: string;
+    user_id: number;
 }
 
 const Replies: React.FC<RepliesProps> = ({ replayedComments }) => {
+    const { user } = useUserHook()
     return (
         replayedComments?.map((item: itemProps, index) => {
             return (
-                <div className='replayed_comment' key={index}>
-                    <p className='replayed_comment_text'>{item.text}</p>
-                    <span className='replay_dots'></span>
-                </div>
+                <div className='replayed_comment_parent' key={index}>
+
+                    <div className='replayed_comment'>
+                        <p className='replayed_comment_text'>{item.text}</p>
+                    </div>
+
+                    
+                        <span className='dots_for_comments_settings space'>
+                            {item.user_id === user.userID  && dotsForComments}
+                        </span>
+                    
+                </div> 
             )
         })
     )
