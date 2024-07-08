2aac65d5789c83c084780cc68a666165bd6443fd
tornike.despotashvili@gmail.com <tornike.despotashvili@gmail.com>
2024-07-08 23:50:13 +0400

    update quiz res

--- src/components/singlePostComp/StandartQuizComponent.tsx
+++ src/components/singlePostComp/StandartQuizComponent.tsx
@@ -22,7 +22,7 @@ interface StandartQuizComponentProps {
   avatar: string;
 }
 
-const StandartQuizComponent: React.FC<StandartQuizComponentProps> = ({avatar, postUserId, image, postID, authorName, date, postTitle, quiz_id }) => {
+const StandartQuizComponent: React.FC<StandartQuizComponentProps> = ({ avatar, postUserId, image, postID, authorName, date, postTitle, quiz_id }) => {
   const { user } = useUserHook()
   const [isUserLogged, setUserLoged] = useState(false)
   const [isHovered, setIsHovered] = useState(false)
@@ -37,7 +37,11 @@ const StandartQuizComponent: React.FC<StandartQuizComponentProps> = ({avatar, po
 
 
   const btnHoverHandler = () => {
-    setIsHovered(true)
+    if (window.matchMedia('(hover: hover)').matches) {
+      setIsHovered(true)
+    } else {
+      setIsHovered(false)
+    }
   }
 
   const returnBtn = () => {
@@ -48,28 +52,28 @@ const StandartQuizComponent: React.FC<StandartQuizComponentProps> = ({avatar, po
     <div className='StandartQuizComponent'>
       {isUserLogged ? <EditPannel postID={postID} type={4} /> : <></>}
 
-      <Link to={'/pages/Quiz/'+quiz_id} className="quiz_link">
-      <div className="quiz_cover_container">
-        {
-          image
-            ? <>
-              <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_bg" />
-              <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_img" />
-            </>
-            : <img src={quizDefaultCover} alt="quiz cover" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+      <Link to={'/pages/Quiz/' + quiz_id} className="quiz_link">
+        <div className="quiz_cover_container">
+          {
+            image
+              ? <>
+                <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_bg" />
+                <img src={image ? `${import.meta.env.VITE_EASY_QUIZ_IMG}${image}.webp` : quizDefaultCover} alt="quiz cover" className="quiz_cover_img" />
+              </>
+              : <img src={quizDefaultCover} alt="quiz cover" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
 
-        }
-      </div>
+          }
+        </div>
       </Link>
 
       <div className="quiz_details">
-        <Author name={authorName} date={date} avatar={avatar}/>
+        <Author name={authorName} date={date} avatar={avatar} />
         <p className="quiz_title">{postTitle}</p>
       </div>
 
       <div className="quiz_btn_link" onMouseOver={btnHoverHandler} onMouseLeave={returnBtn}>
-          <span className={!isHovered ? "q_b_2 active" : "q_b_2"}><ButtonFIlled text={"Get started!"} link={'/pages/Quiz/'+quiz_id} /></span> 
-          <span className={isHovered ? "q_b_1" : "q_b_1 inactive"}><ButtonArrow link={'/pages/Quiz/'+quiz_id} /></span>  
+        <span className={!isHovered ? "q_b_2 active" : "q_b_2"}><ButtonFIlled text={"Get started!"} link={'/pages/Quiz/' + quiz_id} /></span>
+        <span className={isHovered ? "q_b_1" : "q_b_1 inactive"}><ButtonArrow link={'/pages/Quiz/' + quiz_id} /></span>
       </div>
 
       <span className="quiz_popcorn_icon">{popcornIcon}</span>
